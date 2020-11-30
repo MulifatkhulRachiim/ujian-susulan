3083af6b6f174c23f106d644510cd2a6a1690e4c
MulifatkhulRachiim <73068079+MulifatkhulRachiim@users.noreply.github.com>
2020-12-01 00:34:57 +0700

    Add files via upload

---
+++ ujiansusulan.py
@@ -0,0 +1,13 @@
+"C:\Pelita Bangsa_farach\Bahasa Pemrograman\ujiansusulan.py"
+print("Ujian Susulan")
+
+a=0
+while true :
+    bilangan = int(input("Masukan Bilangan : 10"))
+    if (a < bilangan):
+        a=bilangan
+    if (bilangan == 0) :
+        break
+
+print("bilangan terbesar adalah : 100",a)
+print()
